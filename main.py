3e5d66e767f4fc0e79ddf8bf3ef09aeede091c32
EmilioKolo <emilio_193@hotmail.com>
2024-06-28 03:45:45 -0300

    Inicializo main

---
+++ main.py
@@ -0,0 +1,49 @@
+
+# Generales
+import os 
+import time 
+import copy 
+import math 
+import sys 
+import pandas as pd 
+import numpy as np 
+
+# Para display
+from random import shuffle 
+
+# Graficos
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import pandas as pd
+
+
+'''
+Funciones para generar un grafico de cantidad de variantes en una ventana de nucleotidos
+'''
+
+#################################### VARIABLES ####################################
+
+### Variables main()
+
+# Path main depende de posicion actual
+path_git_main = 'C:\\Users\\Admin\\Documents\\Scripts\\Git\\GraficoVentanasGV\\'; 
+path_output_dump_main = 'D:\\ArchivosDoctorado\\Output_dump\\'; 
+path_in_main = path_git_main + ''; ### COMPLETAR
+path_out_main = path_output_dump_main + ''; ### COMPLETAR
+
+
+#################################### FUNCIONES ####################################
+
+
+def _main():
+    # Funcion para probar funciones en ejecucion del archivo
+    return ''
+
+
+#################################### RESULTADOS ###################################
+
+output_dump = []; 
+
+if __name__=='__main__':
+    output_dump.append(_main()); 
+
